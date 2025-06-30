77f97ec62b30f2f322a6edd5848f2519ff728bed
Andersen <17602305274@163.com>
2025-06-30 22:37:35 +0800

    feat: 支持重置云盘密码功能灵活开关

--- shareus-bot/shareus-bot-robot/src/main/java/top/shareus/bot/robot/job/RestGroupMetaPwdTask.java
+++ shareus-bot/shareus-bot-robot/src/main/java/top/shareus/bot/robot/job/RestGroupMetaPwdTask.java
@@ -1,6 +1,8 @@
 package top.shareus.bot.robot.job;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import top.shareus.bot.common.pojo.dto.ResetMetaPasswordDTO;
 import top.shareus.bot.robot.service.AlistService;
@@ -9,11 +11,12 @@ import javax.annotation.Resource;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(name = "alist.reset_password.flag", havingValue = "true", matchIfMissing = true)
 public class RestGroupMetaPwdTask {
 	@Resource
 	private AlistService alistService;
 	
-	// @Scheduled(cron = "0 0 17 * * ?")
+	@Scheduled(cron = "0 0 17 * * ?")
 	public void run() {
 		log.info("开始重置云盘群文件元数据密码……");
 		ResetMetaPasswordDTO metaPasswordDTO = new ResetMetaPasswordDTO();
